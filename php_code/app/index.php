2e31568c22eb6d2bbf1bd18adcf4fe5269d734b6
acmourao <amourao@stj.jus.br>
2023-12-11 19:50:15 -0300

    novos diretorios do projeto

---
+++ php_code/app/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Teste Lista Usuários com Store Procedures</title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <?php
+    require dirname(__FILE__) . '/../dao/usuarios.php';
+    
+    $result = new Dao();
+
+    //      foreach($result->buscaUser('andersson moura') as $row){
+    foreach ($result->buscaAllUsers() as $row) {
+        printf("%s %s %s <br/>", $row['id'], $row['nome'], $row['email']);
+    }
+    $result = null;
+    ?>
+</body>
+
+</html>
--- php_code/index.php
+++ php_code/index.php
@@ -1,20 +1,3 @@
-<!doctype html>
-<html>
-    <head>
-        <title>Teste Lista Usuários com Store Procedures</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-    <?php
-        require dirname(__FILE__) . '/usuarios.php';
-
-        $result = new Dao();
-
-//      foreach($result->buscaUser('andersson moura') as $row){
-        foreach($result->buscaAllUsers() as $row){
-            printf("%s %s %s <br/>", $row['id'], $row['nome'], $row['email']);
-        }
-        $result = null;
-    ?>
-    </body>
-</html>
+<?php
+phpinfo(INFO_ALL);
+?>
