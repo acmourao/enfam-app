88de098f3eaabf59047718613d86c5d978e99d69
acmourao <amourao@stj.jus.br>
2024-01-14 20:55:30 -0400

    view usuario

---
+++ mysql/vwUsuarios.sql
@@ -0,0 +1,14 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`%` 
+    SQL SECURITY DEFINER
+VIEW `vwUsuarios` AS
+    SELECT 
+        `usuarios`.`id` AS `id`,
+        `usuarios`.`nome` AS `nome`,
+        `usuarios`.`email` AS `email`,
+        `usuarios`.`cpf` AS `cpf`,
+        `usuarios`.`telefone` AS `telefone`
+    FROM
+        `usuarios`
+    LIMIT 100
