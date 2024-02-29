247b06766f50749edd9627d900a6c0a2018ee8d3
acmourao <amourao@stj.jus.br>
2024-02-29 16:53:30 -0300

    IRPF fim data import

--- exemplos/IRPF2023_BPFDEC.php
+++ exemplos/IRPF2023_BPFDEC.php
@@ -9,7 +9,11 @@ try {
     //$conexao->beginTransaction();
 
     $filename = "C:\Users\amourao\Documents\BPFDEC.txt";
-    $sql = "SELECT cpf FROM irpf.2023 where cpf= ?";
+    $findCPF = "SELECT cpf FROM irpf.2023 where cpf= ?";
+
+    $RTRT = 0;
+    $RTIRF = 0;
+    $RTPO = 0;
 
     // obtém todas as linhas do arquivo
     $linhas = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -17,59 +21,57 @@ try {
     foreach ($linhas as $num_linha => $linha) {
         $row = explode("|", $linha);
         $total = 0;
-        if ($row[0] == "BPFDEC") {
-            //CPF + NOME
-            //$CPF = $row[1];
-            $CPF = '00061536407';
-            $NOME = $row[2];
-            var_dump($CPF, $NOME);
-            $RTRT = 0;
-            $RTIRF = 0;
-            $RTPO = 0;
-            $stmt = $conexao->prepare($sql);
-            $stmt->execute([$CPF]);
-            $lista = $stmt->fetchAll();
-            if ($lista == NULL) {
-                $sql = "INSERT INTO irpf.2023 (cpf, nome, RTRT, RTIRF, RTPO) VALUES (?, ?, ?, ?, ?)";
-            } else {
-                $sql = "UPDATE irpf.2023 SET RTRT = ?, RTIRF = ?, RTPO = ? WHERE cpf = ?";
+        if (($row[0] == "BPFDEC") || ($row[0] == "FIM")) {
+
+            if (($RTRT > 0 || $RTIRF > 0 || $RTPO > 0) && $num_linha > 0) {
+                $stmt = $conexao->prepare($sql);
+                if (substr($sql, 0, 3) == "INS") {
+                    $stmt->execute([$CPF, $NOME, $RTRT, $RTIRF, $RTPO]);
+                } else {
+                    $stmt->execute([$RTRT, $RTIRF, $RTPO, $CPF]);
+                }
+                echo $sql;
+                //var_dump([$CPF, $NOME, $RTRT, $RTPO, $RTIRF]);
+                $RTRT = 0;
+                $RTPO = 0;
+                $RTIRF = 0;
             }
-            var_dump($sql, $lista);
-            die();
+            if ($row[0] != "FIM") {
+                //CPF + NOME
+                $CPF = $row[1];
+                $NOME = $row[2];
+                var_dump($CPF, $NOME);
+                $stmt = $conexao->prepare($findCPF);
+                $stmt->execute([$CPF]);
+                $lista = $stmt->fetchAll();
+                if ($lista == NULL) {
+                    $sql = "INSERT INTO irpf.2023 (cpf, nome, RTRT, RTPO, RTIRF) VALUES (?, ?, ?, ?, ?)";
+                } else {
+                    $sql = "UPDATE irpf.2023 SET RTRT = ?, RTPO = ?, RTIRF = ? WHERE cpf = ?";
+                }
+                var_dump($sql);
+            }
         } else {
             foreach ($row as $chave => $valor) {
                 if ($valor != 0) {
                     $total += (int) $valor;
                 }
             }
-            var_dump($row[0], $total);
+            var_dump($row[0]);
             if ($total > 0) {
                 if ($row[0] == "RTRT") {
                     $RTRT = $total * .01;
                 }
-                if ($row[0] ==  "RTIRF") {
-                    $RTIRF = $total * .01;
-                }
                 if ($row[0] ==  "RTPO") {
                     $RTPO = $total * .01;
                 }
+                if ($row[0] ==  "RTIRF") {
+                    $RTIRF = $total * .01;
+                }
             }
         }
-        var_dump($RTRT, $RTIRF, $RTPO);
-
-        if ($num_linha > 30) {
-            die();
-        }
-
-        //        $result[] = [$CPF, $NOME, number_format($total * .01, 2, ',', '.')];
     }
-
-    //     $sql = "UPDATE data SET Age='28' WHERE id=201"; 
-    //            $sql = "INSERT INTO irpf.2023 (cpf, nome, RTRT, RTIRF, RIDAC) VALUES (?, ?, 0, 0, ?)";
-    //            $stmt = $conexao->prepare($sql);
-    //            $stmt->execute([$CPF, $NOME, $RIDAC]);
-
-    //    $conexao->commit();
+    $conexao->commit();
 
     echo "Deu certo... Dados atualizados pela ..: " . $dsn;
 } catch (Exception $ex) {
