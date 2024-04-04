cfe7c7d3f47f47072dac83f6c7a586a067163ccd
acmourao <amourao@stj.jus.br>
2024-04-04 14:12:38 -0300

    app atualiza

--- mysql/query user table manutençao.sql
+++ mysql/query user table manutençao.sql
@@ -1,3 +1,15 @@
+SELECT * FROM educaenfam.usuarios where cpf = '30595010253';
+
+SELECT * FROM educaenfam.usuario_roles where usuario_id = 87305;
+
+INSERT INTO educaenfam.usuario_roles (usuario_id,role_id) VALUES (87305, 1);
+
+SELECT * FROM educaenfam.usuario_roles where role_id = 1;
+
+SELECT * FROM educaenfam.usuario_roles where usuario_id = 87305;
+
+
+
 update educaenfam.usuarios SET nome = substring(email,1,LOCATE('@',email)-1) where nome = CPF;
 
 update educaenfam.usuarios SET nome = substring(email,1,LOCATE('@',email)-1) where id in (108620,98099,116789);
