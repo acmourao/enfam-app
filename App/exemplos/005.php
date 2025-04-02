3cf73587e46328b25181b4470d0c74b37a546774
acmourao <amourao@stj.jus.br>
2025-04-02 20:38:35 -0300

    saida em json do post

--- App/exemplos/005.php
+++ App/exemplos/005.php
@@ -2,7 +2,6 @@
 header('Content-type: application/json');
 
 $nome = isset($_GET['nome']) && is_string($_GET['nome']) ? $_GET['nome'] : null;
-
 $lista_json = array('nome' => $nome, 'email' => $_GET['email']);
 
 echo json_encode($lista_json, JSON_PRETTY_PRINT);
